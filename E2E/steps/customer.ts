10e007f2c28cd34077b9232c4016c1bbf8bc1ad4
Flippie Coetser <flippie.coetser@gmail.com>
2017-11-12 19:26:30 +0000

    Update comments

--- E2E/steps/customer.ts
+++ E2E/steps/customer.ts
@@ -1,10 +1,10 @@
 // ***************************************************************************
-// * Customers
+// * Test Customer
 // ***************************************************************************
 import { Customer } from "../../demo/interfaces/Customer";
 export let customer: Customer = {
         Id: "abad1dea-f046-0000-0012-bb3ece8090ce",
-        Name: "Adrian Piels Test #1",
+        Name: "TestCustomer",
         CreateDate: "2017-05-02T07:52:57.555Z",
         EditDate: "2017-05-04T07:52:57.555Z",
         IsDeleted: false,
