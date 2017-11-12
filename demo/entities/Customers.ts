08cb5dea34b976d3c365db8a960eeee6c3b34068
Flippie Coetser <flippie.coetser@gmail.com>
2017-11-12 17:20:20 +0000

    Update type info

--- demo/entities/Customers.ts
+++ demo/entities/Customers.ts
@@ -1,10 +1,10 @@
-import { OData } from "../../src/OData";
+import * as request from "request";
 import { EntitySet } from "../../src/EntitySet";
 import { Customer } from "../interfaces/Customer";
 
 export class Customers extends EntitySet<Customer> {
-    constructor(OData: OData) {
-        super(OData);
+    constructor(options: request.CoreOptions) {
+        super(options);
         this.name = this.constructor.name;
     }
 }
