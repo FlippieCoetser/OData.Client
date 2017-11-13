f2a6a27b28d923f6ec5cfda0d9a350ed858f3444
Flippie Coetser <flippie.coetser@gmail.com>
2017-11-13 17:30:14 +0000

    Implement retrieve.customer scenario

--- E2E/features/create.customer.feature
+++
@@ -1,6 +0,0 @@
-@API
-Feature: Customer
-    Scenario: Create.Customer
-        Given customer does not exist
-        When I create a customer
-        Then customer should exist
--- E2E/steps/customers.ts
+++ E2E/steps/customers.ts
@@ -7,23 +7,48 @@ import { API } from "./api";
 @Cucumber
 export class Customers {
     private api: OData;
+    private entity;
+    private entitySet;
+    private retrievedEntity: any;
     @Before("@API")
-    public async before() {
+    public async beforeAPI() {
         this.api = await API.get();
     }
-    @Given("customer does not exist")
-    public async customerShouldNotExist() {
-        await this.api.customers.delete(customer.Id);
-        expect((await this.api.customers.retrieve(customer.Id)).Id).eql(undefined);
+
+    @Before("@Customer")
+    public beforeCustomer() {
+        this.entity = customer;
+        this.entitySet = "customers";
     }
 
-    @When("I create a customer")
-    public async creatCustomer() {
-        await this.api.customers.create(customer);
+    @Given(/^a clean api$/)
+    public async clean() {
+        return await this.api[this.entitySet].delete(this.entity.Id);
+    }
+    @Given(/^([^"]*)? does not exist$/)
+    public async entityShouldNotExist(entityName: string) {
+        let entity = await this.api[this.entitySet].retrieve(this.entity.Id);
+        expect(entity.Id).eql(undefined);
     }
 
-    @Then("customer should exist")
-    public async customerShouldExist() {
-        expect((await this.api.customers.retrieve(customer.Id)).Id).eql(customer.Id);
+    @When(/^I create a ([^"]*)?$/)
+    public async creatEntity(entityName: string) {
+        await this.api[this.entitySet].create(this.entity);
+    }
+
+    @When(/^I retrieve a ([^"]*)?$/)
+    public async retrieveEntity(entityName: string) {
+        this.retrievedEntity = await this.api[this.entitySet].retrieve(this.entity.Id);
+    }
+
+    @Then(/^([^"]*)? should exist$/)
+    public async entityShouldExist(entityName: string) {
+        let entity = await this.api[this.entitySet].retrieve(this.entity.Id);
+        expect(entity.Id).eql(this.entity.Id);
+    }
+
+    @Then(/^([^"]*)? should be returned$/)
+    public async entityShouldBeReturned(entityName: string) {
+        expect(this.retrievedEntity.Id).eql(this.entity.Id);
     }
 }
