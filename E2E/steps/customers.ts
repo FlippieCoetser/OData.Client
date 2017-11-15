4ac59327360c2accee3b3bf35a97cd04b5b5cb1d
Flippie Coetser <flippie.coetser@gmail.com>
2017-11-15 18:37:28 +0000

    Add sample customer

--- E2E/steps/customers.ts
+++ E2E/steps/customers.ts
@@ -1,17 +1,28 @@
-import { customer } from "./customer";
-import { API } from "./api";
+// ***************************************************************************
+// * Test Customer
+// ***************************************************************************
+import { API, OData } from "./api";
+import { Customer } from "../../demo/interfaces/Customer";
+
 export class Customers {
     constructor() {
         this.name = "Customers";
         this.property = {
             Name: "CustomerName",
         };
-        this.dummy = customer;
+        this.dummy = {
+            Id: "abad1dea-f046-0000-0012-bb3ece8090ce",
+            Name: "TestCustomer",
+            CreateDate: "2017-05-02T07:52:57.555Z",
+            EditDate: "2017-05-04T07:52:57.555Z",
+            IsDeleted: false,
+            IsModified: false,
+        };
     }
-    private api;
+    private api: OData;
     public name: string;
     public property;
-    public dummy;
+    public dummy: Customer;
     public temp;
     public async init() {
         this.api = await API.get();
